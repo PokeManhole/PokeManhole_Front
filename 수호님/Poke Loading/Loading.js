9495030686fd7ac29253acda9b215ea91ecd2115
BSH02 <manon195@naver.com>
2024-05-23 21:17:14 +0900

    Add files via upload

---
+++ 수호님/Poke Loading/Loading.js
@@ -0,0 +1,19 @@
+var image = document.querySelector('.loading-image');
+var percent = document.querySelector('.percent');
+var progress = document.querySelector('.progress');
+var count = 0;
+var per = 0;
+var loading = setInterval(animate, 50);
+
+function animate() {
+  if (count >= 100) {
+    image.src = "pikachu_finish.png";
+    clearInterval(loading);
+  } else {
+    per += 4.06; 
+    count++;
+    progress.style.width = per + 'px';
+    percent.textContent = count + '%';
+    image.style.left = (per - 50) + 'px'; 
+  }
+}
