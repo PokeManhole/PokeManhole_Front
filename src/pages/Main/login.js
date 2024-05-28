fdc143fe79505f5b9cfcc523a3342087d8bcbfe1
조민수 <63090043+toswm128@users.noreply.github.com>
2024-05-28 12:42:12 +0900

    [ADD] login 성공 반응 추가

--- src/pages/Main/login.js
+++ src/pages/Main/login.js
@@ -14,19 +14,34 @@ signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
 
-function init() {
-  loginForm.addEventListener("submit", () => {
-    fetch("http://127.0.0.1:5000/login", {
+const tryLogin = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/login", {
       headers: { "Content-Type": "application/json" },
       method: "POST",
       body: JSON.stringify({
         email: loginEmailInput.value,
         password: loginPasswordInput.value,
       }),
-    })
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  });
+    }).then((res) => res.json());
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+const submitLogin = async (e) => {
+  e.preventDefault();
+  const data = await tryLogin();
+  if (data) {
+    localStorage.setItem("token", data.token);
+    container.style = "display:none";
+  }
+};
+
+function init() {
+  loginForm.addEventListener("submit", (e) => submitLogin(e));
 }
 
 init();
